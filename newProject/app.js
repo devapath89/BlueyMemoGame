c3d83fa6130777fee8d17995fe5fc4acceb330fb
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-01-12 14:18:59 +0100

    gg
    hhh

--- newProject/app.js
+++ newProject/app.js
@@ -1,9 +1,21 @@
 
-
+let clickedCard =null;
 
 function onCardClicked(e) {
     const target = e.currentTarget;
    console.log('clicked',e.currentTarget) 
-    target.className = target.className.replace('color-hidden','').trim();
+   target.className = target.className.replace('color-hidden','').trim();
+   clickedCard =target;
+    
+    // if we havent clicked a card, keep track of the card, display its charater
+    if (!clickedCard){
+        target.className = target.className.replace('color-hidden','').trim();
+        clickedCard =target;
+
+    }else if (clickedCard) {
+
+
+    }
+    //if we have allready clicked a card, check if the new card matches the old card
 
 }
