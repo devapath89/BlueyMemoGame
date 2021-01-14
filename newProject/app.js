87c46233a404a85034e3cb8df1baac3d7d85d5ee
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-01-14 15:13:16 +0100

    3d flip
    added an flip 3d

--- newProject/app.js
+++ newProject/app.js
@@ -1,10 +1,9 @@
 
-// scripts.js
 const cards = document.querySelectorAll('.memo-Card');//storing all memo-Card in a classlist//
 
-function flipCard() {
-  this.classList.toggle('flip'); //     console.log('I was clicked!'); //     console.log(this);
+function flipCard() { //     console.log('I was clicked!'); //     console.log(this);
 
+  this.classList.toggle('flip');  //adding and removing Flip as class through classList//
 
 }
 
