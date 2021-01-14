3d55c1d2b6d463d60668ace172bb1bf8c0d1c548
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-01-14 22:21:59 +0100

    d

--- newProject/app.js
+++ newProject/app.js
@@ -1,25 +1,38 @@
 
+
 const cards = document.querySelectorAll('.memo-Card');//storing all memo-Card in a classlist//
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
 
-function flipCard() { //     console.log('I was clicked!'); //     console.log(this);
-
+function flipCard() { 
   this.classList.add('flip')  //adding and removing Flip as class through
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-  }else{
-      //second card //
-      hasFlippedCard =false
-      secondCard =this; 
-
-      console.log ({firstCard, secondCard})
+     
+    flipped = document.getElementsByClassName('flipped');
+            if(flipped[0].dataset.framework == flipped[1].dataset.framework){
+                console.log("java");
+            }
+      
+    //do cards match ?
+   if (firstCard.dataset.framework==
+    secondCard.dataset.framework && firstCard.dataset.framework != "") { 
+        
+        //its a match 
+        firstCard.removeEventListener('click', flipCard);
+   secondCard.removeEventListener('click', flipCard);
+  } else { 
+    //   console.log("test");
+     //not a match 
+    firstCard.classList.remove('flip');
+    // console.log (firstCard.classList);
+    secondCard.classList.remove('flip');
   }
+       
+  
 }
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
