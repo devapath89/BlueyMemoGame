11ba5bfb6c4bc1a6df39a728fbf2526dcc3638db
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-01-14 15:30:36 +0100

    addinf
    function flipedcard

--- newProject/app.js
+++ newProject/app.js
@@ -1,11 +1,25 @@
 
 const cards = document.querySelectorAll('.memo-Card');//storing all memo-Card in a classlist//
 
+let hasFlipCard = false //added varieable hasFliped//
+let firstCard, secondCard;
+
+
+
+
+
 function flipCard() { //     console.log('I was clicked!'); //     console.log(this);
 
-  this.classList.toggle('flip');  //adding and removing Flip as class through classList//
+  this.classList.add('flip');  //adding and removing Flip as class through classList//
+  if (!hasFlippedCard) {
+      //first Card//
+    hasFlippedCard = true;
+    firstCard = this;
+  }
+ }
+    console.log({hasFlippedCard, firstCard});
+ 
 
-}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
