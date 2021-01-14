fc36ce4d81be409d047b18cbb0f014d9b713739a
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-01-14 18:01:20 +0100

    Update app.js

--- newProject/app.js
+++ newProject/app.js
@@ -1,25 +1,11 @@
 
 const cards = document.querySelectorAll('.memo-Card');//storing all memo-Card in a classlist//
 
-let hasFlipCard = false //added varieable hasFliped//
-let firstCard, secondCard;
-
-
-
-
-
 function flipCard() { //     console.log('I was clicked!'); //     console.log(this);
 
-  this.classList.add('flip');  //adding and removing Flip as class through classList//
-  if (!hasFlippedCard) {
-      //first Card//
-    hasFlippedCard = true;
-    firstCard = this;
-  }
- }
-    console.log({hasFlippedCard, firstCard});
- 
+  this.classList.toggle('flip');  //adding and removing Flip as class through
 
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
