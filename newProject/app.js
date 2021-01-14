9551c026860efed1d74d73f7f3bd0781dd523991
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-01-14 20:29:24 +0100

    added
    added data to class . click to card and adeed if and else if

--- newProject/app.js
+++ newProject/app.js
@@ -1,12 +1,26 @@
 
 const cards = document.querySelectorAll('.memo-Card');//storing all memo-Card in a classlist//
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
 function flipCard() { //     console.log('I was clicked!'); //     console.log(this);
 
-  this.classList.toggle('flip');  //adding and removing Flip as class through
+  this.classList.add('flip')  //adding and removing Flip as class through
 
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  }else{
+      //second card //
+      hasFlippedCard =false
+      secondCard =this; 
+
+      console.log ({firstCard, secondCard})
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-
+ 
